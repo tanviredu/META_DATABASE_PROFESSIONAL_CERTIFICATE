72b592cc6ae677cd5d98dfae7a0b98784efd720e
tanvir_bespoke <tanvir.bespoke@gmail.com>
2022-09-15 19:52:48 +0600

    Added

--- 1_COURSE_INTRODUCTION_TO_DATABASE/SQL_SCRIPT/WEEK2/Solution.sql
+++ 1_COURSE_INTRODUCTION_TO_DATABASE/SQL_SCRIPT/WEEK2/Solution.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE TestDB;
+DROP DATABASE TestDB;
 -- CREATE DATABASE cm_devices;
 USE cm_devices;
 -- SHOW TABLES;
@@ -11,6 +13,32 @@ USE cm_devices;
 SHOW COLUMNS FROM address;
 -- CREATE TABLE invoice(customerName VARCHAR(50),orderDATE DATE,quantity INT,price DECIMAL);
 SHOW COLUMNS FROM invoice;
+
+
+-- SQL ALTER STATEMENT
+-- for adding column
+ALTER TABLE table_name ADD(column_name DATATYPE)
+ALTER TABLE students ADD(age INT,country VARCHAR(50),nationality VARCHAR(250);
+
+-- for removing column 
+ALTER TABLE table_name DROP COLUMN column_name
+ALTER TABLE students DROP COLUMN nationality;
+
+
+
+-- ALTER TABLE table_name MODIFY column_name DATATYPE(LENGTH)
+-- Modify table 
+ALTER TABLE students MODIFY country VARCHAR(100);
+
+
+-- INSERT STATEMENT
+INSERT INTO table_name(Id,name,age,startdate) VALUES (1,"Tanvir",21,"2020-10-15"); 
+
+
+-- SELECT STATEMENT
+SELECT * FROM table_name;
+
+
 
 [Inshalah Will be Added]
 
