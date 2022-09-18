a0cc2759e82c0d6417405599da0706363fc4385a
tanvir_bespoke <tanvir.bespoke@gmail.com>
2022-09-18 13:20:28 +0600

    added

--- 1_COURSE_INTRODUCTION_TO_DATABASE/SQL_SCRIPT/WEEK2/Solution.sql
+++ 1_COURSE_INTRODUCTION_TO_DATABASE/SQL_SCRIPT/WEEK2/Solution.sql
@@ -39,6 +39,45 @@ INSERT INTO table_name(Id,name,age,startdate) VALUES (1,"Tanvir",21,"2020-10-15"
 SELECT * FROM table_name;
 
 
+CREATE DATABASE IF NOT EXISTS shopping_cart_db;
+USE shopping_cart_db;
+
+CREATE TABLE customer(
+    customer_id  INT,
+    name         VARCHAR(100),
+    address      VARCHAR(255),
+    email        VARCHAR(100),
+    phone        VARCHAR(10),
+
+    PRIMARY KEY (customer_id)
+
+);
+
+
+CREATE TABLE product(
+	product_id INT,
+    name VARCHAR(100),
+    price NUMERIC(8,2),
+    description VARCHAR(255),
+    PRIMARY KEY (product_id)
+);
+
+
+
+CREATE TABLE cart_order(
+	order_id INT,
+    customer_id INT,
+    product_id INT,
+    quantity INT,
+    order_date DATE,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY(product_id) REFERENCES product(product_id)
+);
+
+
+
+
 
 [Inshalah Will be Added]
 
